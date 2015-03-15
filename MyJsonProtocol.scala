f65f1afec2d16487d69c4f1483a5838420bf84fb
Felipe Vega <felipe@viglink.com>
2015-03-14 20:10:35 -0500

    Cofigo base

---
+++ MyJsonProtocol.scala
@@ -0,0 +1,9 @@
+package org.smartjava
+
+import spray.json.DefaultJsonProtocol
+
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val personFormat = jsonFormat3(Person)
+}
+
+case class Person(name: String, fistName: String, age: Long)
